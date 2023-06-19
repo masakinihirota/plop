b9d29a216729b2e3d65fe0b919c08a201fb2e1dd
masakinihirota <masakinihirota@gmail.com>
2023-06-20 00:04:51 +0900

    🎨 update 10 files and delete 20 files

--- src/components/aaa/bbb/Ccc/index.tsx
+++ src/components/aaa/bbb/Ccc/index.tsx
@@ -1,11 +1,10 @@
 import { FC } from 'react';
-import styles from './Ccc.module.css';
 
 type Props = {};
 
 export const Ccc: FC<Props> = (props) => {
   return (
-    <div className={styles.modules}>
+    <div className="">
       <h1>Ccc Component</h1>
     </div>
   );
--- src/components/eee/fff/Jjj/index.tsx
+++ src/components/eee/fff/Jjj/index.tsx
@@ -1,11 +1,10 @@
 import { FC } from 'react';
-import styles from './Jjj.module.css';
 
 type Props = {};
 
 export const Jjj: FC<Props> = (props) => {
   return (
-    <div className={styles.modules}>
+    <div className="">
       <h1>Jjj Component</h1>
     </div>
   );
